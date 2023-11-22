e76bbab747c896112226c1f16baa0174ba1e9ed5
Guilherme Bueno <gui.guimatheus@hotmail.com>
2023-11-22 20:02:04 -0300

    update

--- components/Titulo.jsx
+++ components/Titulo.jsx
@@ -1,9 +1,14 @@
 export default function Titulo(props){
         console.log(props)
-    return (
+    return props.pequeno ? (
         <>
-            <h1>{props.principal}</h1>
-            <h3>{props.secundario}</h3>
+            <p>{props.principal}</p>
+            <p>{props.secundario}</p>
         </>
+    ) : (
+        <>
+        <h1>{props.principal}</h1>
+        <h3>{props.secundario}</h3>
+    </>
     )
 }
